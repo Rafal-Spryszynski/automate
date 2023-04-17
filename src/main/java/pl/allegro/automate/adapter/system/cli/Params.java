8dc6faf77d826efa73ef3c8da6b9e5371c31c8de
Rafal Spryszynski <rafal.spryszynski@allegro.pl>
2023-04-17 13:58:22 +0200

    Replace hardcoded path with user directory.

--- src/main/java/pl/allegro/automate/adapter/system/cli/Params.java
+++ src/main/java/pl/allegro/automate/adapter/system/cli/Params.java
@@ -1,10 +1,10 @@
 package pl.allegro.automate.adapter.system.cli;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.immutables.value.Value;
 import org.immutables.value.Value.Default;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Duration;
 
 @Value.Immutable(singleton = true)
@@ -17,7 +17,7 @@ public interface Params {
 
     @Default
     default Path imagesPath() {
-        return Paths.get("C:\\Users\\rafal.spryszynski\\Desktop\\automate");
+        return SystemUtils.getUserDir().toPath();
     }
 
     @Default
